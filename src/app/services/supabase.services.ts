03b3b4880c24994be49763e3d17ad363aeed30d1
siddhant arora <siddhant_arora@siddhants-MacBook-Air.local>
2026-08-21 21:17:30 +0200

    fix: resolve relative path import for environment keys

---
+++ src/app/services/supabase.services.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../../environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupabaseService {
+  public supabase: SupabaseClient;
+
+  constructor() {
+    
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+  }
+}
---
+++ src/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://cwqvmnocvvdedjvksbuw.supabase.co',
+  supabaseKey: 'sb_publishable_e1_y_k1MygAyy4837pXz0Q_MPlRzF98'
+};
